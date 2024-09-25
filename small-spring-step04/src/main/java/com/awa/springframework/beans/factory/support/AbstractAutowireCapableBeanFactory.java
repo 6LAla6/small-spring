60f821c6a1e33c805cfc0c3d9e3f47fb5044e3ed
“awalalala” <2201950900@QQ.com>
2024-09-25 16:08:07 +0800

    step04-start-work

---
+++ small-spring-step04/src/main/java/com/awa/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -0,0 +1,51 @@
+package com.awa.springframework.beans.factory.support;
+
+import com.awa.springframework.beans.BeansException;
+import com.awa.springframework.beans.factory.config.BeanDefinition;
+
+import java.lang.reflect.Constructor;
+
+/**
+ * @Author: awa
+ * @Description: TODO 实现实例化Bean方法
+ * @DateTime: 2024/9/22 20:15
+ **/
+public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFactory{
+
+    private InstantiationStrategy instantiationStrategy = new CglibSubclassingInstantiationStrategy();
+
+    @Override
+    protected Object createBean(String beanName, BeanDefinition beanDefinition, Object[] args) throws BeansException {
+        Object bean = null;
+        //  实例化Bean
+        try {
+            bean = createBeanInstance(beanDefinition, beanName, args);
+        } catch (Exception e) {
+            throw new BeansException("Instantiation of bean failed", e);
+        }
+        //  添加单例Bean对象
+        addSingleton(beanName, bean);
+        return bean;
+    }
+
+    protected Object createBeanInstance(BeanDefinition beanDefinition, String beanName, Object[] args) {
+        Constructor constructorToUse = null;
+        Class<?> beanClass = beanDefinition.getBeanClass();
+        Constructor<?>[] declaredConstructors = beanClass.getDeclaredConstructors();
+        for (Constructor ctor : declaredConstructors) {
+            if (null != args && ctor.getParameterTypes().length == args.length) {
+                constructorToUse = ctor;
+                break;
+            }
+        }
+        return getInstantiationStrategy().instantiate(beanDefinition, beanName, constructorToUse, args);
+    }
+
+    public InstantiationStrategy getInstantiationStrategy() {
+        return instantiationStrategy;
+    }
+
+    public void setInstantiationStrategy(InstantiationStrategy instantiationStrategy) {
+        this.instantiationStrategy = instantiationStrategy;
+    }
+}
